24454b9c789b4464fad962ba9b3858c239b338f6
kyo <mathis.charretier.42@gmail.com>
2022-05-17 13:06:20 +0000

    main.go

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,6 +1,21 @@
 package models
 
-import(
+import (
 	"github.com/jinzhu/gorm"
 	"github.com/matcharr/go-bookstore/pkg/config"
 )
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
